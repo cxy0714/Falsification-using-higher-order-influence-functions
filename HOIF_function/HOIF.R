ff927dea48234e34e85be34d716461784817e898
Xingyu Chen  0714 <1225778616@qq.com>
2024-12-16 22:23:34 +0800

    Update HOIF.R

--- HOIF_function/HOIF.R
+++ HOIF_function/HOIF.R
@@ -1,26 +1,129 @@
+# clear space
 rm(list = ls())
-# 定义需要检测和导入的包列表
-required_packages <- c(
-  "SMUT"
-)
+# package
+library("SMUT") # for efficient matrix multiplication via eigenMapMatMult()
 
-# 检测并安装缺失的包
-for (pkg in required_packages) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    install.packages(pkg)
+################################################################################
+# Main function using "all" U-statistics definition! i.e. (i_1 \ne i_2 \ne ... \ne i_m)
+# Computes and return 4's estimators from 2-th HOIF to 5-th HOIF estimator. Higher order code is coming soon!
+# Vector_1: a n-dimensional vector containing the treatment residuals (like (Aa -1) )
+# Vector_2: a n-dimensional vector containing the outcome residuals (like (y - b),here no A-weighted)
+# weight : a n-dimensional vector containing the treatment for Weight-ed Gram matrix ( like A or (1-A) )
+# basis: a n*p matrix containing the basis transformations of the confounder.
+# Split: a logistic variable, 
+#        Split == 1 means using sample split to compute the Omega_hat (eHOIF),
+#        Split == 0 means using whole sample to compute the Omega_hat (sHOIF).
+################################################################################
+compute_HOIF_general_all_U <- function(Vector_1, Vector_2, weight, basis, Split) {
+  n <- length(Vector_1)
+  
+  if ( Split == 0) {
+    basis_weight <- basis * weight
+    t_basis_weight <- t(basis_weight)
+    Mat <- eigenMapMatMult(t_basis_weight, basis)
+    L <- chol(Mat)
+    Omega_mat <- chol2inv(L) * n
+    
+    
+    Ker <- eigenMapMatMult(eigenMapMatMult(basis, Omega_mat), t_basis_weight)
+    
+    U_list <- list()
+    results <- calculate_u_statistics(Vector_1 = Vector_1, Vector_2 = Vector_2, A1 = Ker, A2 = Ker, A3 = Ker, A4 = Ker)
+    
+    for (i in 2:(5)) {
+      U_list[[paste0("U_", i)]] <- (-1)^i * results[[i-1]]
+    }
+    
+    
+    BD_list <- Buildingblock_sum_HOIF(U_list, order = 5)
+    
+    return_list <- c(BD_list, U_list)
+    return_list <- combine_results(sHOIF = return_list)
+  } else if ( Split == 1) {
+    
+    
+    # Split indices into two halves: a and b
+    indices <- seq_len(n)
+    set.seed(123)  # For reproducibility
+    indices_a <- sample(indices, round(n / 2))
+    indices_b <- setdiff(indices, indices_a)
+    
+    # Split basis, weight, Vector_1, Vector_2
+    basis_a <- basis[indices_a, ]
+    basis_b <- basis[indices_b, ]
+    weight_a <- weight[indices_a]
+    weight_b <- weight[indices_b]
+    Vector_1_b <- Vector_1[indices_b]
+    Vector_2_b <- Vector_2[indices_b]
+    
+    # Compute for group a
+    basis_weight_a <- basis_a * weight_a
+    t_basis_weight_a <- t(basis_weight_a)
+    Mat_a <- eigenMapMatMult(t_basis_weight_a, basis_a)
+    L_a <- chol(Mat_a)
+    Omega_mat_a <- chol2inv(L_a) * (n / 2)
+    
+    # Compute Ker using Omega_mat_a and basis_b
+    Ker_ab <- eigenMapMatMult(eigenMapMatMult(basis_b, Omega_mat_a), t(basis_weight_a))
+    
+    # Compute results using Ker_ab and Vector_1_b, Vector_2_b
+    results_ab <- calculate_u_statistics(Vector_1 = Vector_1_b, Vector_2 = Vector_2_b, 
+                                         A1 = Ker_ab, A2 = Ker_ab, A3 = Ker_ab, A4 = Ker_ab)
+    
+    U_list_ab <- list()
+    for (i in 2:5) {
+      U_list_ab[[paste0("U_", i)]] <- (-1)^i * results_ab[[i - 1]]
+    }
+    
+    BD_list_ab <- Buildingblock_sum_HOIF(U_list_ab, order = 5)
+    return_list_ab <- c(BD_list_ab, U_list_ab)
+    
+    # Combine results for SH-OIF (split a -> b)
+    return_list_ab <- combine_results(eHOIF = return_list_ab)
+    
+    # Repeat the process, swapping a and b
+    basis_weight_b <- basis_b * weight_b
+    t_basis_weight_b <- t(basis_weight_b)
+    Mat_b <- eigenMapMatMult(t_basis_weight_b, basis_b)
+    L_b <- chol(Mat_b)
+    Omega_mat_b <- chol2inv(L_b) * (n / 2)
+    
+    Ker_ba <- eigenMapMatMult(eigenMapMatMult(basis_a, Omega_mat_b), t(basis_weight_b))
+    
+    results_ba <- calculate_u_statistics(Vector_1 = Vector_1[indices_a], Vector_2 = Vector_2[indices_a], 
+                                         A1 = Ker_ba, A2 = Ker_ba, A3 = Ker_ba, A4 = Ker_ba)
+    
+    U_list_ba <- list()
+    for (i in 2:5) {
+      U_list_ba[[paste0("U_", i)]] <- (-1)^i * results_ba[[i - 1]]
+    }
+    
+    BD_list_ba <- Buildingblock_sum_HOIF(U_list_ba, order = 5)
+    return_list_ba <- c(BD_list_ba, U_list_ba)
+    
+    return_list_ba <- combine_results(eHOIF = return_list_ba)
+    
+    # Average the two return lists
+    return_list <- add_lists(return_list_ab, return_list_ba) 
+    return_list <- lapply(return_list, function(x) x / 2)
+    
   }
-  library(pkg, character.only = TRUE)
+ 
+  return(return_list)
 }
 
 
 ################################################################################
-# Main function using "part" U-statistics definition!
-# Computes and return (order-1)'s estimators from 2-th HOIF to order-th HOIF estimator .
-# Vector_1: a vector containing the treatment residuals (like (Aa -1) )
-# Vector_2: a vector containing the outcome residuals (like (y - b) ,but not (A(y-b))! Here no A-weighted)
-# weight_estimation : a vector containing the treatment for Weight-ed Gram matrix ( like A or (1-A) )
+# Main function using "part" U-statistics definition i.e. (i_1 < i_2 < ... < i_m) 
+# Computes and return any order>2 HOIF estimator,(order-1)'s estimators from 2-th HOIF to order-th HOIF estimator .
+# Vector_1: a n-dimensional vector containing the treatment residuals (like (Aa -1) )
+# Vector_2: a n-dimensional vector containing the outcome residuals (like (y - b) ,but not (A(y-b))! Here no A-weighted)
+# weight : a n-dimensional vector containing the treatment for Weight-ed Gram matrix ( like A or (1-A) )
 # basis: a n*p matrix containing the basis transformations of the confounder.
 # order: number of estimators.
+# Split: a logistic variable, 
+#        Split == 1 means using sample split to compute the Omega_hat (eHOIF),
+#        Split == 0 means using whole sample to compute the Omega_hat (sHOIF).
 ################################################################################
 compute_HOIF_general_part_U <- function(Vector_1, Vector_2, weight, basis, order, split) {
   n <- length(Vector_1)
@@ -129,116 +232,6 @@ compute_HOIF_general_part_U <- function(Vector_1, Vector_2, weight, basis, order
 
 
 ################################################################################
-# Main function using "all" U-statistics definition!
-# Computes and return (order-1)'s estimators from 2-th HOIF to 5-th HOIF estimator .
-# Vector_1: a vector containing the treatment residuals (like (Aa -1) )
-# Vector_2: a vector containing the outcome residuals (like (y - b),here no A-weighted)
-# weight_estimation : a vector containing the treatment for Weight-ed Gram matrix ( like A or (1-A) )
-# basis: a n*p matrix containing the basis transformations of the confounder.
-# Split: a logistic variable, 
-#        Split == 1 means using sample split to compute the Omega_hat (eHOIF),
-#        Split == 0 means using whole sample to compute the Omega_hat (sHOIF).
-################################################################################
-compute_HOIF_general_all_U <- function(Vector_1, Vector_2, weight, basis, Split) {
-  n <- length(Vector_1)
-  
-  if ( Split == 0) {
-    basis_weight <- basis * weight
-    t_basis_weight <- t(basis_weight)
-    Mat <- eigenMapMatMult(t_basis_weight, basis)
-    L <- chol(Mat)
-    Omega_mat <- chol2inv(L) * n
-    
-    
-    Ker <- eigenMapMatMult(eigenMapMatMult(basis, Omega_mat), t_basis_weight)
-    
-    U_list <- list()
-    results <- calculate_u_statistics(Vector_1 = Vector_1, Vector_2 = Vector_2, A1 = Ker, A2 = Ker, A3 = Ker, A4 = Ker)
-    
-    for (i in 2:(5)) {
-      U_list[[paste0("U_", i)]] <- (-1)^i * results[[i-1]]
-    }
-    
-    
-    BD_list <- Buildingblock_sum_HOIF(U_list, order = 5)
-    
-    return_list <- c(BD_list, U_list)
-    return_list <- combine_results(sHOIF = return_list)
-  } else if ( Split == 1) {
-    
-    
-    # Split indices into two halves: a and b
-    indices <- seq_len(n)
-    set.seed(123)  # For reproducibility
-    indices_a <- sample(indices, round(n / 2))
-    indices_b <- setdiff(indices, indices_a)
-    
-    # Split basis, weight, Vector_1, Vector_2
-    basis_a <- basis[indices_a, ]
-    basis_b <- basis[indices_b, ]
-    weight_a <- weight[indices_a]
-    weight_b <- weight[indices_b]
-    Vector_1_b <- Vector_1[indices_b]
-    Vector_2_b <- Vector_2[indices_b]
-    
-    # Compute for group a
-    basis_weight_a <- basis_a * weight_a
-    t_basis_weight_a <- t(basis_weight_a)
-    Mat_a <- eigenMapMatMult(t_basis_weight_a, basis_a)
-    L_a <- chol(Mat_a)
-    Omega_mat_a <- chol2inv(L_a) * (n / 2)
-    
-    # Compute Ker using Omega_mat_a and basis_b
-    Ker_ab <- eigenMapMatMult(eigenMapMatMult(basis_b, Omega_mat_a), t(basis_weight_a))
-    
-    # Compute results using Ker_ab and Vector_1_b, Vector_2_b
-    results_ab <- calculate_u_statistics(Vector_1 = Vector_1_b, Vector_2 = Vector_2_b, 
-                                         A1 = Ker_ab, A2 = Ker_ab, A3 = Ker_ab, A4 = Ker_ab)
-    
-    U_list_ab <- list()
-    for (i in 2:5) {
-      U_list_ab[[paste0("U_", i)]] <- (-1)^i * results_ab[[i - 1]]
-    }
-    
-    BD_list_ab <- Buildingblock_sum_HOIF(U_list_ab, order = 5)
-    return_list_ab <- c(BD_list_ab, U_list_ab)
-    
-    # Combine results for SH-OIF (split a -> b)
-    return_list_ab <- combine_results(eHOIF = return_list_ab)
-    
-    # Repeat the process, swapping a and b
-    basis_weight_b <- basis_b * weight_b
-    t_basis_weight_b <- t(basis_weight_b)
-    Mat_b <- eigenMapMatMult(t_basis_weight_b, basis_b)
-    L_b <- chol(Mat_b)
-    Omega_mat_b <- chol2inv(L_b) * (n / 2)
-    
-    Ker_ba <- eigenMapMatMult(eigenMapMatMult(basis_a, Omega_mat_b), t(basis_weight_b))
-    
-    results_ba <- calculate_u_statistics(Vector_1 = Vector_1[indices_a], Vector_2 = Vector_2[indices_a], 
-                                         A1 = Ker_ba, A2 = Ker_ba, A3 = Ker_ba, A4 = Ker_ba)
-    
-    U_list_ba <- list()
-    for (i in 2:5) {
-      U_list_ba[[paste0("U_", i)]] <- (-1)^i * results_ba[[i - 1]]
-    }
-    
-    BD_list_ba <- Buildingblock_sum_HOIF(U_list_ba, order = 5)
-    return_list_ba <- c(BD_list_ba, U_list_ba)
-    
-    return_list_ba <- combine_results(eHOIF = return_list_ba)
-    
-    # Average the two return lists
-    return_list <- add_lists(return_list_ab, return_list_ba) 
-    return_list <- lapply(return_list, function(x) x / 2)
-    
-  }
- 
-  return(return_list)
-}
-
-
-################################################################################
 # Helper funciton for compute_HOIF_general_part_U()
 # Computes and returns (order)-th HOIF estimator and IF from buildingblock U_list
 ################################################################################
@@ -249,7 +242,7 @@ Buildingblock_sum_HOIF <- function(U_list, order, fplugin = 0) {
     BD_value <- 0
 
     for (i in 0:(j - 2)) {
-      # 计算组合数 binom(j-2, i)
+
       combination <- choose(j - 2, i)
 
       U_index <- j - i
@@ -257,14 +250,14 @@ Buildingblock_sum_HOIF <- function(U_list, order, fplugin = 0) {
       BD_value <- BD_value + combination * U_list[[paste0("U_", U_index)]]
     }
 
-    # 存储计算出的 BD 值
+ 
     BD_list[[paste0("IF_", j)]] <- BD_value
   }
 
-  # 初始化一个新的 list 来保存累加结果
+
   sum_list <- list()
 
-  # 计算累加结果
+
   for (i in 2:order) {
     sum_list[[paste0("HOIF", i)]] <- sum(unlist(BD_list[1:(i - 1)])) + fplugin
   }
@@ -273,7 +266,7 @@ Buildingblock_sum_HOIF <- function(U_list, order, fplugin = 0) {
 }
 ################################################################################
 # Main body function in compute_HOIF_general_all_U()
-# Computes and return 4 estimators from 2-th HOIF to 5-th U-statistics.
+# Computes and return 4 building_block estimators from 2-th HOIF to 5-th U-statistics.
 # \bbU_{n, m}^{\mathsf{all}} \biggl[ f(O_{i_1}, \ldots, O_{i_m}) \biggr] = \frac{1}{\binom{n}{m} \factorial{m} } \sum_{i_1 \ne i_2 \ne \ldots \ne i_m} f(O_{i_1}, \ldots, O_{i_m})
 ################################################################################
 calculate_u_statistics <- function(Vector_1, Vector_2, A1, A2 , A3, A4) {
@@ -368,24 +361,24 @@ add_lists <- function(list1, list2) {
   }, list1, list2, SIMPLIFY = FALSE)
 }
 combine_results <- function(...) {
-  # 获取所有输入的列表
+
   input_lists <- list(...)
   list_names <- names(input_lists)
 
-  # 检查是否所有输入都有名字
+
   if (is.null(list_names) || any(list_names == "")) {
     stop("All input lists must be named")
   }
 
-  # 处理每个输入列表，使用输入名字作为前缀
+
   processed_lists <- lapply(names(input_lists), function(list_name) {
     current_list <- input_lists[[list_name]]
-    # 确保当前输入是列表
+
     if (!is.list(current_list)) {
       current_list <- list(current_list)
     }
 
-    # 添加前缀
+
     result <- lapply(names(current_list), function(name) {
       current_list[[name]]
     })
@@ -394,46 +387,55 @@ combine_results <- function(...) {
     return(result)
   })
 
-  # 合并所有处理后的列表
+
   result <- do.call(c, processed_lists)
   return(result)
 }
 
 ################## Example ######################
+# In this example, we estimate the bias of DML/AIPW for the potential outcomes Y(a = 1) and Y(a = 0).
+# The outcome model assumes a linear relationship: Y = A + beta * X.
+# The propensity score model follows a logistic regression: A = psi(alpha * X).
+#
+# - compute_HOIF_general_all_U(): Provides the exact formula for HOIF, but only up to the 5th order.
+# - compute_HOIF_general_part_U(): Computes an approximate formula for HOIF, extendable to any order.
+#       - in their return results, "_HOIF_" are the used estimator, "_IF_" or "_U_" are just middle terms.
+# Note: The basis function of X is the identity in this example. For practical applications, 
+#       it is recommended to apply transformations (e.g., B-splines, Fourier basis, etc.) to X.
+################################################
 
 set.seed(123)
 
-# 单独赋值每个变量
-n <- 1000
-p <- 20
 
+n <- 1000
+p <- 100
 
 mu_x <- 0
 alpha <- rnorm(p, mean = 0, sd = 0.1)
 beta <- rnorm(p, mean = 0, sd = 0.1)
 
 X <- matrix(rnorm(n * p, mu_x, sd = 1), nrow = n, ncol = p)
-# 生成A，A是logistic二元变量
+
 logit_prob <- X %*% alpha
 prob_A <- 1 / (1 + exp(-logit_prob))
 A <- as.numeric(rbinom(n, size = 1, prob = prob_A))
 
-# 生成Y，Y是关于A和X的线性模型
+
 Y <- as.numeric(A + X %*% beta + rnorm(n, mean = 0, sd = 0.2))
 
-# 估计propensity score (A=1的概率)
+
 propensity_model <- glm(A ~ X, family = binomial(link = "logit"))
 propensity_scores <- predict(propensity_model, type = "response")
 propensity_scores <- as.numeric(propensity_scores)
 
-# 估计Y关于A和X的线性模型
+
 Y_model <- lm(Y ~ A + X)
 
-# 预测Y_pred_1和Y_pred_0
+
 Y_pred_1 <- predict(Y_model, newdata = data.frame(A = 1, X = X))
 Y_pred_0 <- predict(Y_model, newdata = data.frame(A = 0, X = X))
 
-# 输出结果
+
 summary(propensity_model)
 summary(Y_model)
 
